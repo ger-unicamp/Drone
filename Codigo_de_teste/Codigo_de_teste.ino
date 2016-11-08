fe2919e875d4a184ea2cab4a9f5b545af5fe5f6e
Bruno Eduardo <bruno97br@gmail.com>
2016-11-08 19:22:12 -0200

    Update Codigo_de_teste.ino

--- Codigo_de_teste/Codigo_de_teste.ino
+++ Codigo_de_teste/Codigo_de_teste.ino
@@ -1,9 +1,3 @@
-/*
-  Configurar ESCs:
-  O codigo abaixo envia um pulso pwm proporcional a entrada analogica de um potenciometro, controlando os quatro escs simultameamente
-  O objetivo principal  calibrar os escs de forma que todos fiquem iguais
-  Ultima atualizaçao: 25/10/2016 - Bruno Eduardo S. de Oliveira
-*/
 
 #include <Servo.h>
 #include <NewPing.h>
@@ -20,28 +14,40 @@
 #define ALTURA 5                          // Altura (em CM) para estabilizar
 
 #define TEMPOATUALIZA 100
-#define TEMPODEVOO 10000
+#define TEMPODEVOO 60000
 
 Servo motores[QUANT_MOTOR];               // Cria um vetor de objetos servo, com tamanho QUANT_MOTOR
-int pinosPwm[] = {3, 5, 6, 9, 10, 11};    // Vetor com os pinos que possuem funçao pwm
+int pinosPwm[] = {
+  3, 5, 6, 9, 10, 11};    // Vetor com os pinos que possuem funçao pwm
 
 NewPing sensor(PINOTRIG, PINOECHO, 200);  // Sensor da newping
 
 int val;
 
-void setup() {
-  void ligar();
+void ligar();
+void decolar();
+void estabilizar();
+void pousar();
+void desligar();
+
+void setup()
+  {
+  pinMode(13, OUTPUT);
+  ligar();
   val = RODA;
   decolar();
+  //Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
   estabilizar();
   pousar();
-}
+  }
 
-void loop() {
+void loop()
+  {
   desligar();
-}
+  }
 
-void ligar() {
+void ligar()
+  {
   delay(10000);
   for (int i = 0; i < QUANT_MOTOR; i++)
     motores[i].attach(pinosPwm[i]);     // Anexa os motores com os pinos pwm. ATENÇAO: OS PINOS ESTAO EM ORDEM CRESCENTE
@@ -53,52 +59,73 @@ void ligar() {
 
   for (int i = 0; i < QUANT_MOTOR; i++)
     motores[i].write(RODA);             //Começa a rodar os motores
-}
-
-void decolar() {
-  while (val < LIMITE) {
-    for (int i = 0; i < QUANT_MOTOR; i++) {
-      motores[i].write(val);
-    }
-    delay(1000);
-    val += PASSO;        //Incrementa a rotação dos motores pelo valor de PASSO
   }
 
+void decolar()
+  {
+  while (val < LIMITE) 
+    {
+    for (int i = 0; i < QUANT_MOTOR; i++) 
+      {
+      motores[i].write(val);
+      }
+    delay(1000);
+    val += PASSO;     //Incrementa a rotação dos motores pelo valor de PASSO
+    }
+  digitalWrite(13, HIGH);
   val -= PASSO;
-}
+  }
 
-void estabilizar() {
+void estabilizar()
+  {
   //Todo
   int count = 0;
-  while(count < (TEMPODEVOO/TEMPOATUALIZA)){
-    int alturaLida = sensor.ping_cm();
+  while(count < (TEMPODEVOO/TEMPOATUALIZA))
+    {
+    int alturaLida = sensor.convert_cm(sensor.ping_median(5));
+    //Serial.print("Ping: "); Serial.print(alturaLida); Serial.println("cm");
     if(alturaLida < ALTURA)
+      {
       val++;
+      digitalWrite(13, HIGH);  //Led acesso subindo
+      }
     else if(alturaLida > ALTURA)
+      {
       val--;
+      digitalWrite(13, LOW); //Led apago descendo
+      }
+    for (int i = 0; i < QUANT_MOTOR; i++) 
+      {
+      motores[i].write(val);
+      }
     delay(TEMPOATUALIZA);
     count++;
-  }
-      
-  
-}
-
-void pousar() {
-  while (val > 0) {
-    for (int i = 0; i < QUANT_MOTOR; i++) {
-      motores[i].write(val);
     }
+  }
+
+void pousar()
+  {
+  while (val > 0) 
+    {
+    for (int i = 0; i < QUANT_MOTOR; i++)
+      {
+      motores[i].write(val);
+      }
     delay(1000);
-    val -= 1;        //Delisga os motores devagar
+    //val -= 1;       //Delisga os motores devagar
+    val = 0;          //Delisga os motores de uma vez
     if (val < 80)
       val = 0;
-  }
-}
-
-void desligar() {
-  while (1) {
-    for (int i = 0; i < QUANT_MOTOR; i++) {
-      motores[i].write(0);    //Encerra os ESCs e motores
     }
   }
-}
+
+void desligar()
+  {
+  while (1) 
+    {
+    for (int i = 0; i < QUANT_MOTOR; i++) 
+      {
+      motores[i].write(0);    //Encerra os ESCs e motores
+      }
+    }
+  }
