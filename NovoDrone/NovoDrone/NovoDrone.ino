01a26ea6ce27b6736f90f867798c2b8a81515841
Elton Cardoso do Nascimento <eltsu3110@hotmail.com>
2019-06-08 15:46:44 -0300

    Criado novo código para o drone (organização)

---
+++ NovoDrone/NovoDrone/NovoDrone.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		NovoDrone.ino
+ Created:	08/06/2019 15:46:17
+ Author:	Elton
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
